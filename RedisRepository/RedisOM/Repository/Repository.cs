4add29536643392469ffc2e7f14be368d66a0bb2
ebrahimkhodadadi <ebrahimkhodadadideveloper@gmail.com>
2023-01-23 04:38:14 +0330

    remove timespan

--- RedisRepository/RedisOM/Repository/Repository.cs
+++ RedisRepository/RedisOM/Repository/Repository.cs
@@ -46,7 +46,7 @@ public class Repository<T> : IRepository<T> where T : class, IEntity<T>, new()
             versionAbleEntity.Version += 1;
         }
 
-        string id = await _redisCollection.InsertAsync(entity, TimeSpan.FromSeconds(10));
+        string id = await _redisCollection.InsertAsync(entity);
         await IncrementEntityCount();
         entity.Id = id;
         return new Result<T>()
